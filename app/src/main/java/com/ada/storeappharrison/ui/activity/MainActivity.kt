db04de50ca5da7edaad50bff3b5e79110fddd386
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2023-05-08 23:43:12 -0500

    Version 1 API con la busqueda de product por Id de MongoDB

--- app/src/main/java/com/ada/storeappharrison/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ada/storeappharrison/ui/activity/MainActivity.kt
@@ -3,12 +3,16 @@ package com.ada.storeappharrison.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.ada.storeappharrison.R
 import com.ada.storeappharrison.databinding.ActivityMainBinding
 import com.ada.storeappharrison.network.dto.product.ProductDto
 import com.ada.storeappharrison.network.service.ProductsService
+import com.ada.storeappharrison.storage.room.dao.ProductDao
+import com.ada.storeappharrison.storage.room.entity.Product
 import com.ada.storeappharrison.ui.viewmodel.MainActivityViewModel
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -22,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var productsService: ProductsService
 
+    @Inject
+    lateinit var productDao: ProductDao
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +37,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
 
-        val idProduct = "6459a2eb7fa14d501011999f"
+        //val idProduct = "6459a2eb7fa14d501011999f"
 
-        requestProductData(idProduct)
+        setClickListeners()
+        addLiveDataObservers()
     }
 
     private fun addLiveDataObservers() {
         viewModel.productQueryResultLiveData.observe(this) { productDto ->
-
+            updateProductSearchResults(
+                productDto
+            )
         }
     }
+
+    private fun setClickListeners() {
+        binding.searchButton.setOnClickListener {
+            viewModel.queryProductsData(binding.searchQuery.text.toString())
+            requestProductData(binding.searchQuery.text.toString())
+        }
+    }
+
 
     private fun requestProductData(idProduct: String) {
         GlobalScope.launch {
@@ -47,7 +65,46 @@ class MainActivity : AppCompatActivity() {
             if (response.isSuccessful){
                 Log.d("AndroidKotlinAda", "Response product with id $idProduct = ${response.body()}")
                 val responseProduct: ProductDto? = response.body()
-                //saveProduct(responseProduct)
+                saveProduct(responseProduct)
+            }
+        }
+    }
+
+    private fun saveProduct(productDto: ProductDto?){
+        GlobalScope.launch {
+            val product = Product(
+                productDto?.category,
+                productDto?.description,
+                productDto?.imageUrl,
+                productDto?.name,
+                productDto?.price
+            )
+
+            productDao.insertAllProducts(product)
+            Log.d("AndroidKotlinAda", "Product saved with success")
+        }
+    }
+
+    private fun updateProductSearchResults(productDto: ProductDto?) {
+        Log.d("AndroidKotlinAda", "Resultado producDto: $productDto")
+        runOnUiThread {
+            if (productDto == null) {
+                Log.d("AndroidKotlinAda", "producDto es null: $productDto")
+                binding.productName.text = getText(R.string.no_product_found)
+                binding.productDescription.visibility = View.INVISIBLE
+                binding.productPrice.visibility = View.INVISIBLE
+                binding.productPoster.visibility = View.INVISIBLE
+
+            } else {
+
+                binding.productName.text = productDto.name
+                binding.productDescription.visibility = View.VISIBLE
+                binding.productPrice.visibility = View.VISIBLE
+                binding.productPoster.visibility = View.VISIBLE
+                binding.productDescription.text = productDto.description
+                binding.productPrice.text = "$ ${productDto.price.toString()}"
+                Glide.with(application).load(productDto.imageUrl)
+                    .into(binding.productPoster)
             }
         }
     }
--- app/src/main/java/com/ada/storeappharrison/ui/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/ada/storeappharrison/ui/viewmodel/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.ada.storeappharrison.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -16,16 +17,24 @@ class MainActivityViewModel
     val productQueryResultLiveData = MutableLiveData<ProductDto?>()
 
     fun queryProductsData(parameter: String) {
+        Log.d("AndroidKotlinAda", "Valor de parameter: $parameter")
         viewModelScope.launch {
             val response = productsService.getProducts(parameter)
+            Log.d("AndroidKotlinAda", "response : $response")
             if (response.isSuccessful) {
+                Log.d("AndroidKotlinAda", "Invocacion product exitosa: $parameter")
                 val product = response.body()
-                if (product == null) {
+                Log.d("AndroidKotlinAda", "varlor de product : $product")
+                if (product!!.name == null) {
+                    Log.d("AndroidKotlinAda", "Entra null interno")
                     productQueryResultLiveData.postValue(null)
                 } else {
+                    Log.d("AndroidKotlinAda", "adiciona product")
                     productQueryResultLiveData.postValue(product)
                 }
             } else {
+                Log.d("AndroidKotlinAda", "Invocacion product no exitosa: $parameter")
+                productQueryResultLiveData.postValue(null)
                 println(response.errorBody())
             }
         }
